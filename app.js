c5b7d86f2872b8934be4b0b57cc31546fb6e20bc
Marco <marco_carrillo@yahoo.com>
2020-01-23 08:11:27 -0500

    Added html as part of engineer/intern/manager class, created non-validated data input and output workflows

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
-function getInfo(role,message){
+// ***************************************************************************/
+// The following function will create the questions to be asked to the user  */
+// based on the team member role                                             */
+//****************************************************************************/
+function getInfo(role){
 
-    (async function(){
+    let qName='';
+    switch (role) {
+        case 'Manager':
+            qName='office';
+            break;
+        case 'Intern':
+            qName='School';
+            break;
+        case 'Engineer':
+            qName='Github'
+            break;
+    }
 
-        try{
-            process.stdout.write('\033c');
-            console.log(message);
-            response=await inquirer.prompt({message:'what is going on',name:'nothing'})
-            return response;
-        }catch(e){console.log(e)}
+    let questions=[{name:'name' ,type:'input',message:`${role}'s name  ? :`},
+                    {name:'id'   ,type:'input',message:`${role}'s id    ? :`},
+                    {name:'email',type:'input',message:`${role}'s email ? :`},
+                    {name:'xyz'  ,type:'input',message:`${role}'s ${qName}? :`}]
 
-    })();
+    return questions;
 }
-
 
 
 //*************************************************************************/
@@ -47,15 +59,53 @@ const Manager=require('./lib/Manager');
                                choices:['Yes','No'],name:'response'}];
             
             conf=await inquirer.prompt(questions);
-            console.log(conf);
         } while (conf.response!=='Yes');
 
-        //********************************************************************** */
-        // confirming the composition of the team so that we can start questions */
-        //********************************************************************** */
-        let teamManager = new Manager;
-        getInfo('Manager','Enter information for the manager');
+        //********************************************************/
+        // Asking information about Manager/ Engineers / Intern  */
+        //********************************************************/
+        let teamMembers=[];
+        process.stdout.write('\033c');
+        for (let i=0;i<answers.nbrEngineers+answers.nbrInterns+1;i++){
 
+            if(i===0){
+                    console.log('ENTER INFORMATION FOR THE MANAGER');
+                    information=await inquirer.prompt(getInfo('Manager'));
+                    teamMembers.push(new Manager(information.name,information.id,information.email,information.xyz));
+            } else if (i<=answers.nbrEngineers){
+                    console.log(`ENTER INFORMATION FOR ENGINEER ${i}`);
+                    information=await inquirer.prompt(getInfo('Engineer'));
+                    teamMembers.push(new Engineer(information.name,information.id,information.email,information.xyz));
+                } else {
+                    console.log(`ENTER INFORMATION FOR INTERN ${i-answers.nbrEngineers}`);
+                    information=await inquirer.prompt(getInfo('Intern'));
+                    teamMembers.push(new Intern(information.name,information.id,information.email,information.xyz));
+            }
+        }
+
+        //*****************************************************************/
+        // Generating the file:  Writing the first part of the file       */
+        //*****************************************************************/
+        let divider_start='<div class="row text-center justify-content-center">';     // Row divider so that we can add 3 cards per row
+        let divider_end='</div>';                                                     // Row ending so that we can add 3 cards per row
+        let fileData=fs.readFileSync(__dirname+'/templates/team.html','utf8');        // Reading template
+        let filePieces=fileData.split('{');                                           // { placeholder to add HTML for cards
+        let data=fs.writeFileSync('output/team.html',filePieces[0]+'\n');             // Writing header + Manager card
+
+        //********************************************************************/
+        // Generating the file:  Writing the manager/engineer/intern cards   */
+        // writes a container row every 3 cards to format HTML properly      */
+        //********************************************************************/
+        let cards_written=0;
+        for (let i=0;i<teamMembers.length;i++){
+                let data=fs.appendFileSync('output/team.html',teamMembers[i].getHTMLcode()+'\n');
+                cards_written++;
+                if (cards_written%3===0&&teamMembers.length>cards_written){
+                    let data=fs.appendFileSync('output/team.html',divider_end+'\n'+divider_start+'\n');
+                }
+            };
+
+        data=fs.appendFileSync('output/team.html',filePieces[1]);
 
     } catch(error){console.log('Error while running the application:  ',error)};
 
